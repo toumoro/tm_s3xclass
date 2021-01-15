4232d7c129e6865450492d596d3c7e0c07d1a381
Stéphane Franic <stephane.franic@toumoro.com>
2021-01-15 16:42:22 -0500

    Support TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.7.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
             'aus_driver_amazon_s3' => '*'
         ],
         'conflicts' => [],
